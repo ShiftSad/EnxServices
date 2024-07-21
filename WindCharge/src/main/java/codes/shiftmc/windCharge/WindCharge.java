95d38c8620af3369e13db7fd1e52d26186e7d7c1
ShiftSad <rosalimavictor@gmail.com>
2024-07-21 02:02:24 -0300

    Initial Commit

---
+++ WindCharge/src/main/java/codes/shiftmc/windCharge/WindCharge.java
@@ -0,0 +1,11 @@
+package codes.shiftmc.windCharge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WindCharge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
