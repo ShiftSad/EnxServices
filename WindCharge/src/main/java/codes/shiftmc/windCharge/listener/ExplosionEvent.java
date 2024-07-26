13f73e0c1e1840829ab8aa41407b016e0d7a2952
ShiftSad <rosalimavictor@gmail.com>
2024-07-25 23:59:55 -0300

    Trying to actually increase the knockback

--- WindCharge/src/main/java/codes/shiftmc/windCharge/listener/ExplosionEvent.java
+++ WindCharge/src/main/java/codes/shiftmc/windCharge/listener/ExplosionEvent.java
@@ -1,27 +1,44 @@
 package codes.shiftmc.windCharge.listener;
 
 import codes.shiftmc.windCharge.MainConfiguration;
+import codes.shiftmc.windCharge.WindChange;
+import com.destroystokyo.paper.event.entity.EntityKnockbackByEntityEvent;
 import com.destroystokyo.paper.event.player.PlayerLaunchProjectileEvent;
 import io.papermc.paper.event.entity.EntityKnockbackEvent;
+import io.papermc.paper.event.entity.EntityPushedByEntityAttackEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.WindCharge;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
+
+import java.util.UUID;
+import java.util.WeakHashMap;
+
+import static org.bukkit.event.EventPriority.*;
 
 public class ExplosionEvent implements Listener {
 
-    @EventHandler
+    private final MainConfiguration config = MainConfiguration.getInstance();
+    private final WeakHashMap<UUID, Integer> explosionMultiplier = new WeakHashMap<>();
+
+    @EventHandler(priority = LOWEST)
     public void onKnockback(EntityKnockbackEvent event) {
         if (event.getCause() != EntityKnockbackEvent.Cause.EXPLOSION) return;
-        if (event.getEntity() instanceof WindCharge) {
-            // Play particle
-            MainConfiguration.getInstance().visual.particle().useEffects().forEach(effect ->
-                    effect.spawn(event.getEntity().getLocation())
-            );
-
-            event.setKnockback(event.getKnockback().multiply(
-                    MainConfiguration.getInstance().config.knockbackMultiplier()
-            ));
-        }
+        Bukkit.getScheduler().runTask(WindChange.getPlugin(WindChange.class), () -> {
+            var map = explosionMultiplier.get(event.getEntity().getUniqueId());
+            System.out.println(map + "|" + Bukkit.getCurrentTick());
+            if (map != null && map >= Bukkit.getCurrentTick()) {
+                System.out.println(String.format("""
+                        Multiplier: %s
+                        Velocity: %s -> %s
+                        """,
+                        config.config.knockbackMultiplier(),
+                        event.getEntity().getVelocity(),
+                        event.getEntity().getVelocity().multiply(config.config.knockbackMultiplier())));
+                event.getEntity().setVelocity(event.getKnockback().multiply(config.config.velocityMultiplier()));
+            }
+        });
     }
 
     @EventHandler
@@ -30,6 +47,22 @@ public class ExplosionEvent implements Listener {
             windCharge.setVelocity(windCharge.getVelocity().multiply(
                     MainConfiguration.getInstance().config.velocityMultiplier()
             ));
+        }
+    }
+
+    @EventHandler(priority = HIGHEST)
+    public void onExplode(EntityExplodeEvent event) {
+        // Get all near entities
+        event.getEntity().getNearbyEntities(1.2, 1.2, 1.2).forEach(entity -> {
+            System.out.println(Bukkit.getCurrentTick());
+            explosionMultiplier.put(entity.getUniqueId(), Bukkit.getCurrentTick() + 3);
+        });
+
+        if (event.getEntity() instanceof WindCharge) {
+            // Play particle
+            config.visual.particle().useEffects().forEach(effect -> {
+                effect.spawn(event.getEntity().getLocation());
+            });
         }
     }
 }
